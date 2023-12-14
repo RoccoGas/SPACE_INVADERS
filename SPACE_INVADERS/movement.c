8ea2ef80e8eb7788e7cb5d2b63baf5e7dd194f78
RoccoGas <80708747+RoccoGas@users.noreply.github.com>
2023-12-13 22:15:11 -0300

    funciona update most left & right

--- SPACE_INVADERS/movement.c
+++ SPACE_INVADERS/movement.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 
 
-void enemy_movement_1(enemyStatus enemy[LEVEL1_ROWS][LEVEL1_COLS], unsigned int * enemyDirection, unsigned int* downFlag, enemyStatus* mostLeftEnemy, enemyStatus* mostRightEnemy) { //se usa en ciclos, conviene tener bitmap como parametro
+void enemy_movement_1(enemyStatus enemy[LEVEL1_ROWS][LEVEL1_COLS], unsigned int * enemyDirection, unsigned int* downFlag, enemyStatus mostLeftEnemy, enemyStatus mostRightEnemy) { //se usa en ciclos, conviene tener bitmap como parametro
 	int i, j;
 
 
-	if ((mostRightEnemy->x + ENEMY_WIDTH) >= DISPLAY_WIDTH && *downFlag == 0) { //todos los sprites de los enemigos tienen el mismo tamaño (o al menos el ancho, arreglar que hacemos)
+	if ((mostRightEnemy.x + ENEMY_WIDTH) >= DISPLAY_WIDTH && *downFlag == 0) { //todos los sprites de los enemigos tienen el mismo tamaño (o al menos el ancho, arreglar que hacemos)
 		update_enemy_y(enemy);
 		*enemyDirection = ENEMY_DIRECTION_LEFT;
 		*downFlag = 1;
 	}	
-	else if (mostLeftEnemy->x <= 0 && *downFlag == 0) {                            
+	else if (mostLeftEnemy.x <= 0 && *downFlag == 0) {                            
 		update_enemy_y(enemy);
 		*enemyDirection = ENEMY_DIRECTION_RIGHT;
 		*downFlag = 1;
@@ -80,7 +80,7 @@ void shoot_laser(playerStatus* player, laser_t* laser) {
 	laser->y = PLAYERY;
 }
 
-void update_laser(laser_t* laser, enemyStatus enemy[LEVEL1_ROWS][LEVEL1_COLS], enemyStatus* mostRight, enemyStatus* mostLeft) {
+void update_laser(laser_t* laser, enemyStatus enemy[LEVEL1_ROWS][LEVEL1_COLS], enemyStatus mostRight, enemyStatus mostLeft) {
 	laser->y -= 20;
 	if (laser->y < 0) { // llego al techo
 		laser->moving = false;
@@ -94,14 +94,13 @@ void update_laser(laser_t* laser, enemyStatus enemy[LEVEL1_ROWS][LEVEL1_COLS], e
 				if ((laser->x > enemy[i][j].x) && (laser->x < enemy[i][j].x + ENEMY_WIDTH) && (laser->y > enemy[i][j].y) && (laser->y < enemy[i][j].y + ENEMY_HEIGHT) && (enemy[i][j].alive)) { //Choco
 					laser->moving = false;
 					enemy[i][j].alive = false;
-					if (&enemy[i][j] == mostLeft) {
-						printf("MostLeft before: %p\n", mostLeft);
+					if (&enemy[i][j] == &mostLeft) {
 						update_most_left(enemy, &mostLeft);
-						printf("MostLeft after: %p\n", mostLeft);
+
 
 					}
-					else if (&enemy[i][j] == mostRight) {
-						update_most_right(enemy, mostRight);
+					else if (&enemy[i][j] == &mostRight) {
+						update_most_right(enemy, &mostRight);
 					}
 					return;
 				}
@@ -118,13 +117,13 @@ void draw_laser(laser_t laser) {
 	al_draw_line(laser.x, laser.y, laser.x, laser.y + LASER_LENGTH, RED, 4);
 }
 
-void update_most_right(enemyStatus enemy[LEVEL1_ROWS][LEVEL1_COLS], enemyStatus** mostRight) {
+void update_most_right(enemyStatus enemy[LEVEL1_ROWS][LEVEL1_COLS], enemyStatus* mostRight) {
 	 int i, j; 
 
 	for (i = LEVEL1_COLS - 1; i >= 0; i--) {
 		for (j = 0; j < LEVEL1_ROWS; j++) {
 			if (enemy[j][i].alive) {
-				*mostRight = &enemy[j][i];
+				*mostRight = enemy[j][i];
 				/*printf("UPDATED MOST RIGHT!\n");
 				printf("mostRight from: %p to %p\n", mostRight, &enemy[j][i]);*/
 
@@ -134,12 +133,12 @@ void update_most_right(enemyStatus enemy[LEVEL1_ROWS][LEVEL1_COLS], enemyStatus*
 		}
 	}
 }
-void update_most_left(enemyStatus enemy[LEVEL1_ROWS][LEVEL1_COLS], enemyStatus** mostLeft) {
+void update_most_left(enemyStatus enemy[LEVEL1_ROWS][LEVEL1_COLS], enemyStatus* mostLeft) {
 	int i, j;
 	for (i = 0; i < LEVEL1_COLS; i++) {
 		for (j = 0; j < LEVEL1_ROWS; j++) {
 			if (enemy[j][i].alive) {
-				*mostLeft = &enemy[j][i];
+				*mostLeft = enemy[j][i];
 				/*printf("UPDATED MOST LEFT!\n");
 				printf("mostLeft from: %p to %p\n", *aux, &enemy[j][i]);*/
 
